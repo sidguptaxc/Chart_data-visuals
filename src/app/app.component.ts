a591eb33cbafa388565382beea1212cb32cff34a
sgsidgupta.460@gmail.com <siddhant@jeebly.com>
2022-06-09 00:11:55 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assignment-chart';
+  chartData: any[] = [
+    {data: [], label: 'Profit'},
+    {data: [], label: 'Sales'}
+  ];
+  chartLabels: any[] = [];
+  chartOptions = {
+    responsive: true,
+  };
+  chartColors: any = [
+    {
+      borderColor: 'red',
+    },
+    {
+      borderColor: 'green',
+    },
+  ];
+  chartLegend = true;
+  chartPlugins = [];
+  data: any[] = [];
+
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.http.get("assets/_files/data.json").subscribe(data =>{
+        this.setChart(data);
+    });
+  }
+
+  setChart(data: any) {
+    this.data = data;
+    this.data.forEach(l => {
+      this.chartLabels.push(l.date);
+      this.chartData[0].data.push(l.profit);
+      this.chartData[1].data.push(l.sale);
+    });
+
+    this.cd.detectChanges();
+
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ChartsModule } from '@rinminase/ng-charts';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
